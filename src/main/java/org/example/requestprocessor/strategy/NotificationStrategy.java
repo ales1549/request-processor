9fbf82e0c847138d0388b728b2ecf0b8a7f9258e
Алексей Кантур <ales.zaaz49@gmail.com>
2026-05-25 18:33:39 +0300

    SP-4393

--- src/main/java/org/example/requestprocessor/strategy/NotificationStrategy.java
+++ src/main/java/org/example/requestprocessor/strategy/NotificationStrategy.java
@@ -1,10 +1,6 @@
 package org.example.requestprocessor.strategy;
 
-import org.example.requestprocessor.model.enums.NotificationType;
-
 public interface NotificationStrategy {
 
     void process(String message);
-
-    NotificationType getType();
 }
