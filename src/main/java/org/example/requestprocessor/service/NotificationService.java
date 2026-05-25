4f5310b680f82aaff9524c7fa17615de4341f54d
Алексей Кантур <ales.zaaz49@gmail.com>
2026-05-25 18:49:16 +0300

    SP-4393 fix.

---
+++ src/main/java/org/example/requestprocessor/service/NotificationService.java
@@ -0,0 +1,33 @@
+package org.example.requestprocessor.service;
+
+import lombok.RequiredArgsConstructor;
+import org.example.requestprocessor.model.dto.NotificationRequest;
+import org.example.requestprocessor.model.enums.NotificationType;
+import org.example.requestprocessor.strategy.NotificationStrategy;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class NotificationService {
+
+    private final Map<NotificationType, NotificationStrategy> strategies;
+
+    @Autowired
+    public NotificationService(List<NotificationStrategy> strategyList) {
+        this.strategies = strategyList.stream()
+                .collect(Collectors.toMap(strategy -> strategy.getType(), strategy -> strategy));
+    }
+
+    public void process(NotificationRequest request){
+        NotificationStrategy strategy = strategies.get(request.getType());
+         if(strategy == null){
+             throw new IllegalArgumentException("Неизвестный тип уведомления: " + request.getType());
+         }
+         strategy.process(request.getMessage());
+    }
+}
---
+++ src/main/java/org/example/requestprocessor/strategy/PushStrategy.java
@@ -0,0 +1,44 @@
+package org.example.requestprocessor.strategy;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.example.requestprocessor.model.NotificationOutbox;
+import org.example.requestprocessor.model.enums.NotificationType;
+import org.example.requestprocessor.repository.NotificationOutboxRepository;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class PushStrategy implements NotificationStrategy {
+
+    public final NotificationOutboxRepository notificationRepository;
+
+    @Override
+    public NotificationType getType() {
+        return NotificationType.PUSH;
+    }
+
+    @Override
+    public void process(String message){
+
+        String key = UUID.randomUUID().toString();
+        String body = """
+                {"message": "%s"}
+                """.formatted(message);
+        NotificationOutbox pushOutbox = new NotificationOutbox();
+        pushOutbox.setId(UUID.randomUUID());
+        pushOutbox.setCreatedAt(LocalDateTime.now());
+        pushOutbox.setTopic("push-events");
+        pushOutbox.setKey(key);
+        pushOutbox.setValue(message);
+        pushOutbox.setSent(false);
+        pushOutbox.setAttempt(1);
+
+        notificationRepository.save(pushOutbox);
+        log.info("Подготовлено сообщение для отправки. Key: {}, Payload: {}, topic: {}", key, body, pushOutbox.getTopic());
+    }
+}
